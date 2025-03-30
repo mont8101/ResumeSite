6992be33928563b4a3b47d8040ccce05b4b287cc
mont8101 <55461476+mont8101@users.noreply.github.com>
2025-03-29 20:24:20 -0500

    removed light mode, accessability button focus bug fix

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -15,18 +15,18 @@ export default function Nav() {
 
 
 return(
-    <nav className=' bg-white dark:bg-gray-950 fixed w-full z-20 top-0 start-0 border-b-1 border-gray-200 dark:border-gray-600'>
+    <nav className='bg-gray-950 fixed w-full z-20 top-0 start-0 border-b-1 border-gray-600'>
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between md:justify-center mx-auto p-4">
             <div className='flex flex-col visible md:hidden'>
                 <a href="https://linkedin.com/in/ben-m" className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Ben Montgomery</span>
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Ben Montgomery</span>
                 </a>
-                <div className='self-center text-l font-semibold whitespace-nowrap dark:text-gray-400'>
+                <div className='self-center text-l font-semibold whitespace-nowrap text-gray-400'>
                     Software Consultant
                 </div>
             </div>
             <div className="flex md:order-2 space-x-3 md:space-x-0">
-                    <button type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+                    <button type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm  rounded-lg md:hidden focus:outline-none focus:ring-2  text-gray-400 hover:bg-gray-700 focus:ring-gray-600">
                     <span className="sr-only">Open main menu</span>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                         <path fillRule="evenodd" d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
@@ -34,18 +34,18 @@ return(
                 </button>
             </div>
             <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
-                <button onClick={() => scrollToSection("About")} className="relative inline-flex items-center justify-center p-0.5 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-                    <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-950 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+                <button onClick={() => scrollToSection("About")} className="relative inline-flex items-center justify-center p-0.5 me-2 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 text-white focus:ring-4 focus:outline-none focus:ring-blue-800">
+                    <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-gray-950 rounded-md group-hover:bg-transparent">
                         About
                     </span>
                 </button> 
-                <button onClick={() => scrollToSection("Projects")} className="relative inline-flex items-center justify-center p-0.5 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-blue-500 to-cyan-500 group-hover:from-blue-500 group-hover:to-cyan-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
-                    <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-950 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+                <button onClick={() => scrollToSection("Projects")} className="relative inline-flex items-center justify-center p-0.5 me-2 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-blue-500 to-cyan-500 group-hover:from-blue-500 group-hover:to-cyan-500 text-white focus:ring-4 focus:outline-none focus:ring-cyan-800">
+                    <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-gray-950 rounded-md group-hover:bg-transparent">
                         Project Summary
                     </span>
                 </button>
-                <button onClick={() => scrollToSection("Contact")} className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-green-400 group-hover:from-cyan-500 group-hover:to-green-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
-                    <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-950 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+                <button onClick={() => scrollToSection("Contact")} className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-cyan-500 to-green-400 group-hover:from-cyan-500 group-hover:to-green-400 text-white focus:ring-4 focus:outline-none focus:ring-green-800">
+                    <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-gray-950 rounded-md group-hover:bg-transparent">
                         Contact
                     </span>
                 </button>
